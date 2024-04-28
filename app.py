bd9d421428615db53e471d55a97a78563e91d785
Elikem-Ahlijah <agbenyame@gmail.com>
2024-04-28 11:29:44 +0000

    first commit the whole app

---
+++ app.py
@@ -0,0 +1,23 @@
+# app.py
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to myMedulla App!'
+
+@app.route('/send_request', methods=['POST'])
+def send_request():
+    data = request.json
+    url = data.get('url')
+    
+    if url:
+        response = requests.get(url)
+        return jsonify({'status': 'success', 'data': response.text}), 200
+    else:
+        return jsonify({'status': 'error', 'message': 'URL not provided'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
